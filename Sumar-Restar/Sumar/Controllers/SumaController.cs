b0894a43f14bc1d474781d978aae5ed410ac30f6
Cristhian Arispe Vargas <cristhianarispevargas@gmail.com>
2020-08-15 10:18:15 -0400

    Mi primer modelo de clase

---
+++ Sumar-Restar/Sumar/Controllers/SumaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sumar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Add")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        [HttpPost]
+        [Route("Add")]
+        public int Add2([FromHeader]int a, [FromHeader]int b)
+        {
+            return a + b;
+        }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Mul2([FromHeader]int a, [FromHeader] int b)
+        {
+            return a * b;
+        }
+    }
+}
